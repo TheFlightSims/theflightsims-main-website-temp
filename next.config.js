a5a5085e6bcaf7b55ff7c3a198bd5d2131cdf4c7
白いねこ <shiroineko.tfs@gmail.com>
2024-11-19 23:58:51 +0700

    Update next.config.js
    Signed-off-by: 白いねこ  <shiroineko.tfs@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false, // Disable JavaScript minification
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
+};
 
 module.exports = nextConfig;
