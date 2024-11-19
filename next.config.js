3ff9857d7e992d9672f07991e45a82c194912f02
白いねこ <shiroineko.tfs@gmail.com>
2024-11-20 00:20:38 +0700

    Update next.config.js
    Signed-off-by: 白いねこ  <shiroineko.tfs@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false, // Disable JavaScript minification
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
+  //output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  //skipTrailingSlashRedirect: true,
+  // Optional: Change the output directory `out` -> `dist`
+  //distDir: 'static',
 };
 
 module.exports = nextConfig;
